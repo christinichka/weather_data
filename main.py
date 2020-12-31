392cb3154486b1abd228aeb8d6bdb2da224f6d2a
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2020-12-31 12:48:47 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -19,13 +19,17 @@
 # 	print(data)
 
 
-import csv
+# import csv
 
+# with open("weather_data.csv") as data_file:
+# 	data = csv.reader(data_file)
+# 	temperatures = []
+# 	for row in data:
+# 		if row[1] != "temp":
+# 			temperatures.append(row[1])
+# 	print(temperatures)
 
-with open("weather_data.csv") as data_file:
-	data = csv.reader(data_file)
-	temperatures = []
-	for row in data:
-		if row[1] != "temp":
-			temperatures.append(row[1])
-	print(temperatures)
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data)
